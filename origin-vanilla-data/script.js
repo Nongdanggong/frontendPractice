546f382b8625bba2f104094012f2a8bea798b259
Nongdanggong <ovo3507@naver.com>
2025-10-31 15:05:20 +0900

    원본 데이터 이동

---
+++ origin-vanilla-data/script.js
@@ -0,0 +1,17 @@
+const bindMenus = document.querySelectorAll(".bind-menu");
+
+bindMenus.forEach((menu) => {
+  const subMenuWrapper = menu.querySelector(".sub-menu-wrapper");
+
+  menu.addEventListener("mouseenter", () => {
+    if (subMenuWrapper) {
+      subMenuWrapper.classList.add("smooth-active");
+    }
+  });
+
+  menu.addEventListener("mouseleave", () => {
+    if (subMenuWrapper) {
+      subMenuWrapper.classList.remove("smooth-active");
+    }
+  });
+});
